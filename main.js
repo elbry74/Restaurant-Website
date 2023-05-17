faf1151e5b781f6230bf3b11101839b57257d228
elbry <abdoelbry22@gmail.com>
2023-05-17 15:05:32 +0300

    filter

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
 $(document).ready(function ($) {
     "use strict";
 
+    jQuery(".filters").on("click", function () {
+        jQuery("#menu-dish").removeClass("bydefault_show");
+    });
+    $(function () {
+        var filterList = {
+            init: function () {
+                $("#menu-dish").mixItUp({
+                    selectors: {
+                        target: ".dish-box-wp",
+                        filter: ".filter",
+                    },
+                    animation: {
+                        effects: "fade",
+                        easing: "ease-in-out",
+                    },
+                    load: {
+                        filter: ".all, .breakfast, .lunch, .dinner",
+                    },
+                });
+            },
+        };
+        filterList.init();
+    });
+
     jQuery(".menu-toggle").click(function () {
         jQuery(".main-navigation").toggleClass("toggled");
     });
@@ -26,4 +50,8 @@ $(document).ready(function ($) {
     }
     var scene = $(".js-parallax-scene").get(0);
     var parallaxInstance = new Parallax(scene);
+})
+
+jQuery(window).on('load' , function (){
+    $('body').removeClass('body-fixed');
 })
